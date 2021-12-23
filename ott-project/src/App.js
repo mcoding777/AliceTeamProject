8e60e3e0a0702c1ff83f0d65a61e24436c37fde5
misun <mcoding777@naver.com>
2021-12-23 17:25:39 +0900

    main 페이지에 3개의 구역 추가 (height: 100vh;)

--- ott-project/src/App.js
+++ ott-project/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  // 첫 메인화면 컴포넌트
   function Main() {
     return (
       <main>
@@ -10,10 +12,26 @@ function App() {
             <div className='netflix_logo' />
             <h1>영화 제작사와 투자자 여러분<br />환영합니다</h1>
           </header>
+          <footer>
+            <Button text="시작하기" />
+          </footer>
+        </article>
+        <article className='how_to_use1'>
+          <p>2번째페이지</p>
+        </article>
+        <article className='how_to_use2'>
+          <p>3번째페이지</p>
         </article>
       </main>
-      
     )
+  }
+
+  // 빨간 버튼 컴포넌트
+  function Button(props) {
+    return (
+      <div className='button'>{props.text}</div>
+    )
+
   }
   return (
     <Main />
