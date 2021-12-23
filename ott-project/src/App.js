b1b9e7949c45a61cbd00fbeda3afbc06061e90ab
misun <mcoding777@naver.com>
2021-12-23 16:57:34 +0900

    로고 및 위치 설정

--- ott-project/src/App.js
+++ ott-project/src/App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  function Main() {
+    return (
+      <main>
+        <article className='main'>
+          <header>
+            <div className='netflix_logo' />
+            <h1>영화 제작사와 투자자 여러분<br />환영합니다</h1>
+          </header>
+        </article>
+      </main>
+      
+    )
+  }
   return (
-    <div>
-      <h1>Hello React!</h1>
-    </div>
+    <Main />
   );
 }
 
