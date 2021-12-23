03ed8a684176c523d5f160e90ce716023c792e3f
misun <mcoding777@naver.com>
2021-12-23 20:20:49 +0900

    main 페이지 3번째 영역에 월드맵 이미지 추가

--- ott-project/src/App.js
+++ ott-project/src/App.js
@@ -18,10 +18,18 @@ function App() {
           </footer>
         </article>
         <article className='how_to_use1'>
-          <p>2번째페이지</p>
+          <p>COVID-19로 인해 OTT 플랫폼의 영향력은 더욱 커졌습니다.<br />
+            여러분의 성공적인 영화 제작과 투자를 위해<br />
+            한국 컨텐츠의 영향력을 분석해드립니다.
+          </p>
+          <div className='covid_table'>표 들어갈 곳</div>
         </article>
         <article className='how_to_use2'>
-          <p>3번째페이지</p>
+          <p>우리 서비스는<img src="./netflix-log.png" alt="(넷플릭스 로고)" />한국 컨텐츠를 기준으로 분석합니다.<br />
+            각 나라에서 흥행하는 한국 컨텐츠를 바탕으로<br />
+            효율적인 벤치마킹을 경험해보십시오.
+          </p>
+          <div className='world_map' />
         </article>
       </main>
     )
