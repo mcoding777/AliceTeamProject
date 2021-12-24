ba0c1bca763cfcdce85df868d81a330ecffe1e00
misun <mcoding777@naver.com>
2021-12-24 14:47:38 +0900

    조회 기준 관련한 메뉴 박스 grid 나눔

--- ott-project/src/Page1.js
+++ ott-project/src/Page1.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './css/Page.css';
 
 function Page1() {
     return (
         <main>
-            <article className='main'>
+            <article className='page1'>
                 <div className='grid_container'>
+                    <div className='menubox'>
+                        <p>카테고리별</p>
+                        <div>
+                            <label><input type="checkbox" />영화</label>
+                        </div> 
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
             </article>
         </main>
