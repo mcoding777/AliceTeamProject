fd684ca3f321022eb232153014da5157e2e4d502
misun <mcoding777@naver.com>
2021-12-26 04:03:28 +0900

    MenuBox 컴포넌트에서 Page 컴포넌트로 props 전달 확인

--- ott-project/src/MenuBox.js
+++ ott-project/src/MenuBox.js
@@ -1,17 +1,39 @@
 import './css/MenuBox.css';
+import { useState } from 'react';
 
 function MenuBox(props) {
+    const [category, setCategory] = useState("");
+    const [review, setReview] = useState("");
+
+    function getCategoryValue(event) {
+        setCategory(event.target.value);
+        props.category(event.target.value);
+    }
+
+    function getReviewValue(event) {
+        setReview(event.target.value);
+        props.review(event.target.value);
+    }
+
     return (
         <div className='menubox'>
             <p>CATEGORY</p>
             <div>
-                <label><input type="radio" name="category" /> Movie</label>
-                <label><input type="radio" name="category" /> TV Series</label>
+                <label>
+                    <input type="radio" name="category" value="movie" onClick={getCategoryValue} /> Movie
+                </label>
+                <label>
+                    <input type="radio" name="category" value="tv" onClick={getCategoryValue} /> TV Series
+                </label>
             </div> 
             <p>REVIEW</p>
             <div>
-                <label><input type="radio" name="review" /> Market</label>
-                <label><input type="radio" name="review" /> K-Contents</label>
+                <label>
+                    <input type="radio" name="review" value="market" onClick={getReviewValue} /> Market
+                </label>
+                <label>
+                    <input type="radio" name="review" value="kcontents" onClick={getReviewValue} /> K-Contents
+                </label>
             </div>
             <p className='tip'>
                 CATEGORY는 넷플릭스 컨텐츠를<br />
