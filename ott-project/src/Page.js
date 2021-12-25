f37983f12ef809f46adf569cdf4c51690f3b3a07
misun <mcoding777@naver.com>
2021-12-25 21:32:21 +0900

    메뉴 박스에 사용 방법(tip) 추가

--- ott-project/src/Page.js
+++ ott-project/src/Page.js
@@ -16,7 +16,19 @@ function Page() {
                         <div>
                             <label><input type="checkbox" /> Market</label>
                             <label><input type="checkbox" /> K-Contents</label>
-                        </div> 
+                        </div>
+                        <p className='tip'>
+                            CATEGORY는 넷플릭스 컨텐츠를<br />
+                            크게 2가지 분류로 나눕니다.<br />
+                            Movie : 영화<br />
+                            TV Series : TV쇼(드라마)<br />
+                            <br />
+                            REVIEW는 넷플릭스 시장의 인사이트와<br />
+                            한국 컨텐츠의 영향력을<br />
+                            다양한 지표로 분석해드립니다.<br />
+                            Market : 넷플릭스 시장 현황<br />
+                            K-Contents : 한국 컨텐츠 분석
+                        </p>
                     </div>
                 </div>
             </article>
