d5344480869c09141c3fdd74a42d009c9f0ef0db
misun <mcoding777@naver.com>
2021-12-27 21:16:47 +0900

    수정 : index.js에 있던 컴포넌트를 App.js로 이동

--- ott-project/src/App.js
+++ ott-project/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
 import Main from './Main';
+import Page from './Page';
+import './css/reset.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Main />
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/page" element={<Page />} />
+    </Routes>
   );
 }
 
