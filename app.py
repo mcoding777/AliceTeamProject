bf19f1bde4914c56f252fac7a64eedc7db6874d7
alli-eunbi <allieunbi@gmail.com>
2021-12-30 16:24:00 +0900

    internal server error 계속 발생

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
+from models import HowtoUse
+import pymysql
 from flask import Flask
 from flask_restx import Resource, Api
+from db_connect import db
+from models import HowtoUse
+import config
 
 app = Flask(__name__)
 api = Api(app)
+db.init_app(app)
+app.secret_key = "secret!key"
+app.config.from_object(config)
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
+@api.route('/corona')
+class corona(Resource):
     def get(self):
-        return {'hello': 'world'}
+        data = HowtoUse.query.order_by(HowtoUse.year.desc()).all()
+        return {'corona_list': data}
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,12 +1,36 @@
-import flask from Flask
-import sqlachemy
+from flask import Flask
+from db_connect import db
 
-db = sqlachemy()
 
-# class Movie(db.Model) :
-#     __table__name = 'Netflix'
-#     id  = db.Column(db.Integer, primary_key=True, nullable=False)
-#     name = db.Column(db.String(20), nullable=False)
+class HowtoUse(db.Model):
+    __table__name = 'HowtoUse'
+    year = db.Column(db.Integer, primary_key=True, nullable=False)
+    movie_cost = db.Column(db.Integer, nullable=False)
+    ott_cost = db.Column(db.BigInteger, nullable=False)
 
-# def __init__(self, name):
-#     self.name = name
+    def __init__(self, year, movie_cost, ott_cost):
+        self.year = year
+        self.movie_cost = movie_cost
+        self.ott_cost = ott_cost
+
+
+class Contents(db.Model):
+    __table__name = 'Contents'
+    name = db.Column(db.String(60), primary_key=True, nullable=False)
+    group_name = db.Column(db.String(10), nullable=False)
+    category = db.Column(db.String(10), nullable=False)
+    genre = db.Column(db.String(20), nullable=False)
+    genre2 = db.Column(db.String(20), nullable=False)
+    actors = db.Column(db.String(200), nullable=False)
+    summary = db.Column(db.String(500), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    score = db.Column(db.DECIMAL(3, 2), nullable=False)
+    award = db.Column(db.Integer, nullable=False)
+    global_score = db.Column(db.Integer, nullable=False)
+    popularity = db.Column(db.Integer, nullable=False)
+    total_score = db.Column(db.DECIMAL(3, 2), nullable=False)
+    imdb_url = db.Column(db.String(200), nullable=False)
+    poster_url = db.Column(db.String(200), nullable=False)
+
+    # def __init__(self, name):
+    # self.name = name
