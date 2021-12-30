b73e0b4d62a6ea0fae46ddf5fa2d1e2374f4a438
misun <mcoding777@naver.com>
2021-12-31 02:46:48 +0900

    코로나 데이터 가져오기 성공~~

--- ott-project/src/Main.js
+++ ott-project/src/Main.js
@@ -4,14 +4,87 @@ import './css/Main.css';
 import { Link } from 'react-router-dom';
 import 'chart.js/auto'
 import { Chart } from 'react-chartjs-2'
+import { useEffect, useState } from 'react';
 
 // 첫 메인화면 컴포넌트
 function Main() {
+    const [coronas, setCoronas] = useState([]);
 
     const getCorona = async () => {
         const corona = await fetch(`http://13.58.124.132/corona`);
         const corona_json = await corona.json();
-        console.log(corona_json[0]);
+        setCoronas(corona_json);
+    }
+
+    console.log(coronas);
+
+    useEffect(() => {
+        getCorona()
+    }, []);
+
+    // 코로나 차트
+    function CovidChart() {
+
+        const data = {
+            labels: coronas.map(x => x.years),
+            datasets: [
+              {
+                type: 'line',
+                label: '영화관 매출액',
+                borderColor: '#F05454',
+                borderWidth: 2,
+                data: coronas.map(x => x.movie_cost),
+                tension: 0.5,
+                yAxisID: 'y0', // 축 id
+              },
+              {
+                type: 'line',
+                label: '넷플릭스 매출액',
+                borderColor: '#30475E',
+                borderWidth: 2,
+                data: coronas.map(x => x.ott_cost),
+                tension: 0.5,
+                yAxisID: 'y1',
+              },
+            ],
+          };
+          
+          const options = {
+            scales: { // 축 스타일링
+                y0: {   // 축 id별로 스타일링 객체로 넣기
+                    id: 'y0',
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    grid: { // 축 기준이 되는 grid 스타일링
+                        borderColor: "rgb(54, 162, 235)",
+                      }
+                },
+                y1: {
+                    id: 'y1',
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    grid: {
+                        borderColor: "rgb(255, 99, 132)",
+                      }
+                },
+            },
+            plugins: {
+                legend: { // 범례 스타일링
+                  labels: {
+                    usePointStyle: true,
+                    // 범례 도형 모양과 관련된 속성으로, false일 경우엔 기본 직사각형 도형으로 표시됩니다.
+                    },
+                }
+            }
+        };
+    
+        return (
+            <div className='covid_table'>
+                <Chart type="line" data={data} options={options} />
+            </div>
+        )
     }
 
     return (
@@ -47,68 +120,3 @@ function Main() {
 }
 
 export default Main;
-
-// styled-components
-function CovidChart() {
-
-    const data = {
-        labels: ['2017', '2018', '2019', '2020', '2021(9월까지)'],
-        datasets: [
-          {
-            type: 'line',
-            label: '영화관 매출액',
-            borderColor: '#F05454',
-            borderWidth: 2,
-            data: [219876227, 216385269, 226678777, 59524093, 59540088],
-            tension: 0.5,
-            yAxisID: 'y0', // 축 id
-          },
-          {
-            type: 'line',
-            label: '넷플릭스 매출액',
-            borderColor: '#30475E',
-            borderWidth: 2,
-            data: [11692.71, 15794.34, 20156.45, 24996.06, 21988.53],
-            tension: 0.5,
-            yAxisID: 'y1',
-          },
-        ],
-      };
-      
-      const options = {
-        scales: { // 축 스타일링
-            y0: {   // 축 id별로 스타일링 객체로 넣기
-                id: 'y0',
-                type: 'linear',
-                display: true,
-                position: 'left',
-                grid: { // 축 기준이 되는 grid 스타일링
-                    borderColor: "rgb(54, 162, 235)",
-                  }
-            },
-            y1: {
-                id: 'y1',
-                type: 'linear',
-                display: true,
-                position: 'right',
-                grid: {
-                    borderColor: "rgb(255, 99, 132)",
-                  }
-            },
-        },
-        plugins: {
-            legend: { // 범례 스타일링
-              labels: {
-                usePointStyle: true,
-                // 범례 도형 모양과 관련된 속성으로, false일 경우엔 기본 직사각형 도형으로 표시됩니다.
-                },
-            }
-        }
-    };
-
-    return (
-        <div className='covid_table'>
-            <Chart type="line" data={data} options={options} />
-        </div>
-    )
-}
