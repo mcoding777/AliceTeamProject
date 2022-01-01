776615d631434005196d4463dde62fa6b98cb51c
misun <mcoding777@naver.com>
2022-01-01 17:08:08 +0900

    Kcontents 포스터 이미지 API 데이터로 호출

--- ott-project/src/Kcontents.js
+++ ott-project/src/Kcontents.js
@@ -26,6 +26,7 @@ function Kcontents() {
     const award = Math.floor(Number(totalData.award));
     const global = Math.floor(Number(totalData.global));
     const popularity = Math.floor(Number(totalData.popularity));
+    const poster = totalData.poster;
 
     console.log(score, award, global, popularity);
 
@@ -49,11 +50,11 @@ function Kcontents() {
                     <div className='divContainer'>
                         <DivContainer>
                             <PosterDiv>
-                                <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAyMTA5MDFfODYg%2FMDAxNjMwNDkxNzgwNzk5.Skcb8LU8_yF5jfzocvTQLga6bimMJQhgAjsoT6bOjuMg.fzOpEXQCz3BT7lEQ1EhwQFuIfxO8ajh1wCyqihyBxpwg.JPEG%2FIfe0SYsWK3RCpqK3HxlsdNyy1FQ4.jpg&type=sc960_832" alt="오징어게임" />
-                                <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA1MjFfMjgg%2FMDAxNTkwMDM0Nzc2NDUz.tKQtPs0t9Sl39UitHJ-74WGejOvl5NEUMBHoSEZjA-sg.8QI7n2aoeunLcn5GFcgUGfE6z3GqEYVtMYtD_Adq5YAg.JPEG.dnwjs2015%2F%25BD%25BD%25B1%25E2%25B7%25CE%25BF%25EE%25C0%25C7%25BB%25E7%25BB%25FD%25C8%25B0%25B4%25EB%25BA%25BB%252C%25BF%25A9%25C0%25DA%25B5%25E5%25B6%25F3%25B8%25B6%25B5%25B6%25B9%25E9%252C%25BF%25A9%25C0%25DA%25BF%25AC%25B1%25E2%25B4%25EB%25BB%25E7%252C%25B5%25B6%25B9%25E9%25B4%25EB%25BB%25E7.jpg&type=sc960_832" alt="슬기로운의사생활" />
-                                <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5486%2F2021%2F11%2F15%2F0000191606_002_20211115181002794.jpg&type=sc960_832" alt="지옥" />
-                                <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5401%2F2020%2F04%2F24%2F0000203788_001_20200424173638369.jpg&type=sc960_832" alt="연애의참견" />
-                                <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F382%2F2021%2F11%2F26%2F0000947350_001_20211126091001891.jpg&type=sc960_832" alt="고요의바다" />
+                                <img src={poster[0]} alt="오징어게임" />
+                                <img src={poster[1]} alt="슬기로운의사생활" />
+                                <img src={poster[2]} alt="지옥" />
+                                <img src={poster[3]} alt="연애의참견" />
+                                <img src={poster[4]} alt="고요의바다" />
                             </PosterDiv>
                             <TotalP>
                                 A class 는 종합평점 4점이상으로 전체 컨텐츠중 1% 비중으로 7개의 컨텐츠가 있습니다.
