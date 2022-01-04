cd8fc0fbbc734d824e3752d65284adf5b004e46b
misun <mcoding777@naver.com>
2022-01-04 16:30:20 +0900

    수정 : ClassCard article 컴포넌트화

--- ott-project/src/ClassCard.js
+++ ott-project/src/ClassCard.js
@@ -1,5 +1,6 @@
 import styled, { keyframes } from 'styled-components';
 import { useLocation, useNavigate } from 'react-router-dom';
+import Article from './Article';
 
 function ClassCard() {
 
@@ -15,42 +16,37 @@ function ClassCard() {
 
     return (
         <>
-            <article>
-                <div className='divContainer'>
-                    <ClassDivContainer>
-                        <ClassDiv 
-                            title="A" 
-                            onClick={() => handleGetClass("A")}>
-                            <span>A Class</span>
-                        </ClassDiv>
-                        <ClassDiv 
-                            title="B" 
-                            onClick={() => handleGetClass("B")}>
-                            <span>B Class</span>
-                        </ClassDiv>
-                        <ClassDiv 
-                            title="C" 
-                            onClick={() => handleGetClass("C")}>
-                            <span>C Class</span>
-                        </ClassDiv>
-                        <ClassDiv 
-                            title="D" 
-                            onClick={() => handleGetClass("D")}>
-                            <span>D Class</span>
-                        </ClassDiv>
-                    </ClassDivContainer>
-                </div>
-            </article>
+            <Article>
+                <ClassDivContainer>
+                    <ClassDiv 
+                        title="A" 
+                        onClick={() => handleGetClass("A")}>
+                        <span>A Class</span>
+                    </ClassDiv>
+                    <ClassDiv 
+                        title="B" 
+                        onClick={() => handleGetClass("B")}>
+                        <span>B Class</span>
+                    </ClassDiv>
+                    <ClassDiv 
+                        title="C" 
+                        onClick={() => handleGetClass("C")}>
+                        <span>C Class</span>
+                    </ClassDiv>
+                    <ClassDiv 
+                        title="D" 
+                        onClick={() => handleGetClass("D")}>
+                        <span>D Class</span>
+                    </ClassDiv>
+                </ClassDivContainer>
+            </Article>
         </>
     )
 }
 
-
 export default ClassCard;
 
-
 /* styled-components */
-
 const img1 = "https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAyMTA5MDFfODYg%2FMDAxNjMwNDkxNzgwNzk5.Skcb8LU8_yF5jfzocvTQLga6bimMJQhgAjsoT6bOjuMg.fzOpEXQCz3BT7lEQ1EhwQFuIfxO8ajh1wCyqihyBxpwg.JPEG%2FIfe0SYsWK3RCpqK3HxlsdNyy1FQ4.jpg&type=sc960_832";
 const img2 = "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA1MjFfMjgg%2FMDAxNTkwMDM0Nzc2NDUz.tKQtPs0t9Sl39UitHJ-74WGejOvl5NEUMBHoSEZjA-sg.8QI7n2aoeunLcn5GFcgUGfE6z3GqEYVtMYtD_Adq5YAg.JPEG.dnwjs2015%2F%25BD%25BD%25B1%25E2%25B7%25CE%25BF%25EE%25C0%25C7%25BB%25E7%25BB%25FD%25C8%25B0%25B4%25EB%25BA%25BB%252C%25BF%25A9%25C0%25DA%25B5%25E5%25B6%25F3%25B8%25B6%25B5%25B6%25B9%25E9%252C%25BF%25A9%25C0%25DA%25BF%25AC%25B1%25E2%25B4%25EB%25BB%25E7%252C%25B5%25B6%25B9%25E9%25B4%25EB%25BB%25E7.jpg&type=sc960_832";
 const img3 = "https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5486%2F2021%2F11%2F15%2F0000191606_002_20211115181002794.jpg&type=sc960_832";
