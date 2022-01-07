9086756c5d8b87b8db7bc6ff532272918dbc4d49
misun <mcoding777@naver.com>
2022-01-07 10:35:27 +0900

    수정 : MenuBox에 나타나는 정체불명의 블러 오류 (아마도) 해결

--- ott-project/src/MenuBox.js
+++ ott-project/src/MenuBox.js
@@ -36,71 +36,73 @@ function MenuBox() {
     return (
         <>
             <NavBox>
-                <MenuText>CATEGORY</MenuText>
-                <SelectContainer>
-                    <label>
-                        <input 
-                            type="radio" 
-                            name="category" 
-                            value="movie" 
-                            onClick={getCategoryValue} 
-                            defaultChecked={category === "movie"}
-                        /> Movie
-                    </label>
-                    <label>
-                        <input 
-                            type="radio" 
-                            name="category" 
-                            value="tv-series" 
-                            onClick={getCategoryValue} 
-                            defaultChecked={category === "tv"}
-                        /> TV Series
-                    </label>
-                </SelectContainer> 
-                <MenuText>REVIEW</MenuText>
-                <SelectContainer>
-                    <label>
-                        <input 
-                            type="radio" 
-                            name="review" 
-                            value="market" 
-                            onClick={getReviewValue} 
-                            defaultChecked={review === "market"}
-                        /> Market
-                    </label>
-                    <label>
-                        <input 
-                            type="radio" 
-                            name="review" 
-                            value="k-contents" 
-                            onClick={getReviewValue} 
-                            defaultChecked={review === "kcontents"}
-                        /> K-Contents
-                    </label>
-                </SelectContainer>
-                <TipContainer>
-                    CATEGORY는 넷플릭스 컨텐츠를<br />
-                    크게 2가지 분류로 나눕니다.<br />
-                    Movie : 영화<br />
-                    TV Series : TV쇼(드라마)<br />
-                    <br />
-                    REVIEW는 넷플릭스 시장의 인사이트와<br />
-                    한국 컨텐츠의 영향력을<br />
-                    다양한 지표로 분석해드립니다.<br />
-                    Market : 넷플릭스 시장 현황<br />
-                    K-Contents : 한국 컨텐츠 분석<br />
-                    <br />
-                    데이터 분석 기간 : 2015.01 ~ 2021.02
-                </TipContainer>
-                <ButtonContainer>
-                    <button 
-                        onClick={getHandleResult}>
-                        결과보기
-                    </button>
-                    <Link to="/">
-                        <button>처음으로</button>
-                    </Link>
-                </ButtonContainer>
+                <NavContainer>
+                    <MenuText>CATEGORY</MenuText>
+                    <SelectContainer>
+                        <label>
+                            <input 
+                                type="radio" 
+                                name="category" 
+                                value="movie" 
+                                onClick={getCategoryValue} 
+                                defaultChecked={category === "movie"}
+                            /> Movie
+                        </label>
+                        <label>
+                            <input 
+                                type="radio" 
+                                name="category" 
+                                value="tv-series" 
+                                onClick={getCategoryValue} 
+                                defaultChecked={category === "tv"}
+                            /> TV Series
+                        </label>
+                    </SelectContainer> 
+                    <MenuText>REVIEW</MenuText>
+                    <SelectContainer>
+                        <label>
+                            <input 
+                                type="radio" 
+                                name="review" 
+                                value="market" 
+                                onClick={getReviewValue} 
+                                defaultChecked={review === "market"}
+                            /> Market
+                        </label>
+                        <label>
+                            <input 
+                                type="radio" 
+                                name="review" 
+                                value="k-contents" 
+                                onClick={getReviewValue} 
+                                defaultChecked={review === "kcontents"}
+                            /> K-Contents
+                        </label>
+                    </SelectContainer>
+                    <TipContainer>
+                        CATEGORY는 넷플릭스 컨텐츠를<br />
+                        크게 2가지 분류로 나눕니다.<br />
+                        Movie : 영화<br />
+                        TV Series : TV쇼(드라마)<br />
+                        <br />
+                        REVIEW는 넷플릭스 시장의 인사이트와<br />
+                        한국 컨텐츠의 영향력을<br />
+                        다양한 지표로 분석해드립니다.<br />
+                        Market : 넷플릭스 시장 현황<br />
+                        K-Contents : 한국 컨텐츠 분석<br />
+                        <br />
+                        데이터 분석 기간 : 2015.01 ~ 2021.02
+                    </TipContainer>
+                    <ButtonContainer>
+                        <button 
+                            onClick={getHandleResult}>
+                            결과보기
+                        </button>
+                        <Link to="/">
+                            <button>처음으로</button>
+                        </Link>
+                    </ButtonContainer>
+                </NavContainer>
             </NavBox>
             <main>
                 <Outlet />
@@ -116,19 +118,21 @@ const NavBox = styled.nav`
     width: 19.6vw;
     height: 89vh;
 
+    color: black;
+
     position: fixed;
     top: 5vh;
     bottom: 5vh;
     z-index: 1;
 
     background-color: #981217;
+`;
 
-    padding: 4vh 2vw;
-
-    color: black;
-
+const NavContainer = styled.div`
     display: flex;
     flex-direction: column;
+
+    padding: 4vh 2vw;
 `;
 
 const SelectContainer = styled.div`
