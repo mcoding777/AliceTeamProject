ac981dd1f3935279102d7bf60c3a987c16bb9b1b
misun <mcoding777@naver.com>
2022-01-08 17:19:34 +0900

    수정 : GenreTable 더미데이터 주석 처리

--- ott-project/src/GenreTable.js
+++ ott-project/src/GenreTable.js
@@ -13,56 +13,57 @@ function createData(Genre, Award, Global, Popularity, Score, Total) {
 
 export default function GenreTable({genreTableData}) {
 
-    const dummyData = [
-        {
-          "Drama": {
-            "award": "2.7250",
-            "global": "1.2125",
-            "popularity": "1.7625",
-            "score": "3.51687500",
-            "total_score": "2.30421875"
-          }
-        },
-        {
-          "ets": {
-            "award": "1.7600",
-            "global": "1.7200",
-            "popularity": "1.4000",
-            "score": "3.50400000",
-            "total_score": "2.09600000"
-          }
-        },
-        {
-          "Comedy": {
-            "award": "2.0870",
-            "global": "1.1522",
-            "popularity": "1.6739",
-            "score": "3.46847826",
-            "total_score": "2.09538043"
-          }
-        },
-        {
-          "Action": {
-            "award": "2.5761",
-            "global": "1.5543",
-            "popularity": "2.1739",
-            "score": "3.43913043",
-            "total_score": "2.43586957"
-          }
-        },
-        {
-          "Crime": {
-            "award": "2.5625",
-            "global": "1.4375",
-            "popularity": "2.3750",
-            "score": "3.47187500",
-            "total_score": "2.46171875"
-          }
-        }
-      ]
+    // 더미 데이터
+    // const dummyData = [
+    //     {
+    //       "Drama": {
+    //         "award": "2.7250",
+    //         "global": "1.2125",
+    //         "popularity": "1.7625",
+    //         "score": "3.51687500",
+    //         "total_score": "2.30421875"
+    //       }
+    //     },
+    //     {
+    //       "ets": {
+    //         "award": "1.7600",
+    //         "global": "1.7200",
+    //         "popularity": "1.4000",
+    //         "score": "3.50400000",
+    //         "total_score": "2.09600000"
+    //       }
+    //     },
+    //     {
+    //       "Comedy": {
+    //         "award": "2.0870",
+    //         "global": "1.1522",
+    //         "popularity": "1.6739",
+    //         "score": "3.46847826",
+    //         "total_score": "2.09538043"
+    //       }
+    //     },
+    //     {
+    //       "Action": {
+    //         "award": "2.5761",
+    //         "global": "1.5543",
+    //         "popularity": "2.1739",
+    //         "score": "3.43913043",
+    //         "total_score": "2.43586957"
+    //       }
+    //     },
+    //     {
+    //       "Crime": {
+    //         "award": "2.5625",
+    //         "global": "1.4375",
+    //         "popularity": "2.3750",
+    //         "score": "3.47187500",
+    //         "total_score": "2.46171875"
+    //       }
+    //     }
+    //   ]
     
-    const dummyData_rows = 
-        dummyData.map((item) => {
+    const genreTable = 
+        genreTableData.map((item) => {
           if (item?.Crime) {
             return (
               createData(
@@ -152,7 +153,7 @@ export default function GenreTable({genreTableData}) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {dummyData_rows.map((row) => (
+            {genreTable.map((row) => (
               <TableRow
                 key={row.Genre}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
